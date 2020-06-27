7628073c546ee3c243312102d75e06be26908c0b
Eduardo Escoto <me@eduardo.wtf>
2020-06-26 23:25:49 -0700

    more configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,77 @@
 module.exports = {
-  siteMetadata: {
-    title: `eduardo-exists`,
-    description: `Learn more about Eduardo Escoto here!`,
-    author: `@eduardo-exists`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+	siteMetadata: {
+		title: `eduardo-exists`,
+		description: `Learn more about Eduardo Escoto here!`,
+		author: `@eduardo-exists`,
+		url: `https://eduardo.wtf`,
+	},
+	plugins: [
+		`gatsby-plugin-styled-components`,
+		`gatsby-plugin-root-import`,
+		`gatsby-plugin-react-helmet`,
+		`gatsby-remark-reading-time`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images`,
+			},
+		},
+		`gatsby-transformer-sharp`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				path: `${__dirname}/src/posts`,
+			},
+		}`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-plugin-mdx`,
+			options: {
+				gatsbyRemarkPlugins: [
+					{
+						resolve: `gatsby-remark-images`,
+						options: {
+							backgroundColor: 'transparent',
+							maxWidth: 900,
+							linkImagesToOriginal: false,
+						},
+					},
+					`gatsby-remark-smartypants`,
+				],
+				plugins: [
+					{
+						resolve: `gatsby-remark-images`,
+						options: {
+							backgroundColor: 'transparent',
+							maxWidth: 900,
+							linkImagesToOriginal: false,
+						},
+					},
+				],
+			},
+		},
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `eduardo-exists portfolio`,
+				short_name: `eduardo-exists`,
+				start_url: `/`,
+				background_color: `#663399`,
+				theme_color: `#663399`,
+				display: `minimal-ui`,
+				icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+			},
+		},
+		{
+			resolve: `gatsby-plugin-google-analytics`,
+			options: {
+				anonymize: true,
+				trackingId: 'UA-147372030-1',
+			},
+		},
+		`gatsby-theme-waves`,
+		// this (optional) plugin enables Progressive Web App + Offline functionality
+		// To learn more, visit: https://gatsby.dev/offline
+		// `gatsby-plugin-offline`,
+	],
 }
