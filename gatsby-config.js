c8c9e1c77971c6a117fadd287fe1b023cc24dde5
Eduardo Escoto <me@eduardo.wtf>
2020-06-26 22:19:57 -0700

    refactoring some stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Eduardo-Exists`,
+    description: `Learn more about Eduardo Escoto here!`,
+    author: `@eduardo-exists`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
